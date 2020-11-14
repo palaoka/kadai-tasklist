81ac2c3771bb4cf1ae3eefb2930fe95a806f608b
palaoka <naokiharaoka@gmail.com>
2020-11-14 15:40:03 +0000

    lesson15 kadai1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-    root to: "tasks#index"  
-    resources :tasks
-
-    get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :new, :create]
+    
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: "tasks#index"
+  resources :tasks
+    
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
 end
