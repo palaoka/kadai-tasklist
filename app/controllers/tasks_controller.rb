167150f8a37f5c9d83d48b49c16ea58977e0d042
palaoka <naokiharaoka@gmail.com>
2020-11-17 10:42:52 +0000

    saiteisyutsu

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,19 +2,19 @@ class TasksController < ApplicationController
     before_action :require_user_logged_in
     before_action :set_message, only: [:show, :edit, :update, :destroy]
     
-    def index
-        @tasks = Task.all
+  def index
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
+  end
     
     def show
     end
     
-    def new
-        @task = Task.new
-    end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = "Taskが正常に投稿されました"
@@ -29,9 +29,10 @@ class TasksController < ApplicationController
     end
     
     def update
+        @task = current_user.tasks.build(task_params)
         if @task.update(task_params)
             flash[:success] = "タスクは正常に更新されました"
-            redirect_to @task
+            redirect_to root_url
         else
             flash.now[:danger] = "タスクは更新されませんでした"
             render :edit
